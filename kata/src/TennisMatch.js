20cea271d59e561530c2e49d33db269927bc7389
Rich <rich.hildebrand@gmail.com>
2014-05-16 23:33:42 -0400

    killed dead code

--- kata/src/TennisMatch.js
+++ kata/src/TennisMatch.js
@@ -8,16 +8,6 @@ function(ScoreCalculator, Player) {
    TennisMatch.prototype.PlayerOne = new Player();
    TennisMatch.prototype.PlayerTwo = new Player();
 
-   TennisMatch.prototype.Winner = undefined;
-
-   TennisMatch.prototype.PlayerOneScores = function() {
-      this.ScorePoint(this.PlayerOne);
-   }
-
-   TennisMatch.prototype.PlayerTwoScores = function() {
-      this.ScorePoint(this.PlayerTwo);
-   }
-
    TennisMatch.prototype.GetScore = function() {
       return this.ScoreCalculator.PrintScore(this.PlayerOne.Points, this.PlayerTwo.Points); 
    }
